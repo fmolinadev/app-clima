58e6bb38986d65f20d544e44ef9c910a7da1030e
Francisco Molina <fdm.molina@gmail.com>
2022-06-03 20:43:38 -0300

    Revision in process...

--- src/App.js
+++ src/App.js
@@ -12,9 +12,24 @@ const apiKey = "4ae2636d8dfbdc3044bede63951a019b";
 
 function App() {
   const [cities, setCities] = useState([]);
+
   function onClose(id) {
-    setCities((oldCities) => oldCities.filter((c) => c.id !== id));
-    return swal("Ciudad eliminada");
+    return swal({
+      title: "¿Estás seguto?",
+      text: "Si aceptas, dejaras de ver la informacion de esta ciudad.",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        setCities((oldCities) => oldCities.filter((c) => c.id !== id));
+        swal("Ciudad eliminada de la lista.", {
+          icon: "success",
+        });
+      } else {
+        swal("No se producieron cambios.");
+      }
+    });
   }
   async function onSearch(ciudad) {
     //Llamado a la API del clima
@@ -22,28 +37,29 @@ function App() {
       `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&lang=es&appid=${apiKey}`
     )
       .then((r) => r.json())
-      .then((recurso) => {
-        if (recurso.main !== undefined) {
+      .then((resultado) => {
+        if (resultado.main !== undefined) {
           const ciudad = {
-            min: recurso.main.temp_min,
-            max: recurso.main.temp_max,
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: recurso.main.temp,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon,
+            min: resultado.main.temp_min,
+            max: resultado.main.temp_max,
+            img: resultado.weather[0].icon,
+            id: resultado.id,
+            wind: resultado.wind.speed,
+            temp: resultado.main.temp,
+            name: resultado.name,
+            weather: resultado.weather[0].main,
+            clouds: resultado.clouds.all,
+            latitud: resultado.coord.lat,
+            longitud: resultado.coord.lon,
           };
 
           let ciudadEncontrada = cities.find((city) => city.id === ciudad.id);
+
           if (ciudadEncontrada)
             return swal("La ciudad ya se encuentra en la lista.");
           else return setCities((oldCities) => [...oldCities, ciudad]);
         } else {
-          swal("Ciudad no encontrada");
+          swal("Ciudad no encontrada.");
         }
       });
   }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import './Cards.css';
+import React from "react";
+import "./Cards.css";
 
-import Card from './Card.jsx';
+import Card from "./Card.jsx";
 
-export default function Cards({cities, onClose}) {
+export default function Cards({ cities, onClose }) {
+  // console.log("ENTRO EN CARDS =>", cities);
   return (
-    <div className='cards'>
-      {cities.map(c => <Card
-          key={c.id}
-          id={c.id}
-          max={c.max}
-          min={c.min}
-          name={c.name}
-          img={c.img}
-          onClose={() => onClose(c.id)}
-        /> )}
+    <div className="cards">
+      {cities.length ? (
+        cities.map((c) => (
+          <Card
+            key={c.id}
+            id={c.id}
+            max={c.max}
+            min={c.min}
+            name={c.name}
+            img={c.img}
+            onClose={() => onClose(c.id)}
+          />
+        ))
+      ) : (
+        <h2>¡Comienza buscando una ciudad!</h2>
+      )}
     </div>
   );
 }
